d575a0b5786e81c3bd19ac5fe526fe0a56f3a061
Boldoo Zolbayar <bolderdenez40@gmail.com>
2023-01-22 17:26:29 +0800

    home page

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import "./Home.css";
+import React from "react";
+import home from "../images/home.svg";
+
+function Home () {
+    return (
+    <section className="home">
+        <div className="container">
+            <div className="home-row">
+                <div className="home-1">
+                    <h1 id="home-title">
+                        Эерэг өөрчлөлтөд хөтлөх
+                        <span> сайн дурын ажилд </span>
+                        таныг урьж байна.
+                    </h1>
+                </div>
+                <div className="home-2">
+                    <img src={home}></img>
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+export default Home;
---
+++ src/components/Nav.js
@@ -0,0 +1,33 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo from '../images/logo.png'
+import "./Nav.css";
+
+function Na() {
+  return (
+    <Navbar sticky='top' bg="white" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+            <img id='img' src={Logo}></img>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="container-fluid">
+          <Navbar.Collapse className="justify-content-end">
+                <Nav.Link  href="#home">Нүүр</Nav.Link>
+                <Nav.Link  href="#link">Бидний тухай</Nav.Link>
+                <Nav.Link  href="#link">Холбогдох</Nav.Link>
+                <Nav.Link  href="#link">Элсэх</Nav.Link>
+                <Nav.Link  href="#link">
+                    <button type="button" class="btn btn-outline ">Donation</button>
+                </Nav.Link>
+            </Navbar.Collapse>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Na;
