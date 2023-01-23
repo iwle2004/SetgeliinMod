d4b7734f4fe3731e673031b51c7efb372f24ceb0
iwle2004 <bilguunkhureltulga0@gmail.com>
2023-01-24 00:55:37 +0800

    Updates

---
+++ precache-manifest.91abe38bb55aecd4854333ee6bf0feba.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b974530089951b63b045",
+    "url": "/SetgeliinMod/static/css/main.73c21885.chunk.css"
+  },
+  {
+    "revision": "b974530089951b63b045",
+    "url": "/SetgeliinMod/static/js/main.b9745300.chunk.js"
+  },
+  {
+    "revision": "0b9a35d6bd3e23a2db7c",
+    "url": "/SetgeliinMod/static/css/1.018f8a29.chunk.css"
+  },
+  {
+    "revision": "0b9a35d6bd3e23a2db7c",
+    "url": "/SetgeliinMod/static/js/1.0b9a35d6.chunk.js"
+  },
+  {
+    "revision": "0cd3043ac695b22d4278",
+    "url": "/SetgeliinMod/static/js/runtime~main.0cd3043a.js"
+  },
+  {
+    "revision": "87561d2099a7e58fcd47762769905234",
+    "url": "/SetgeliinMod/static/media/image 2.87561d20.png"
+  },
+  {
+    "revision": "f0d5a2c553787cb7ba79c52db4b6f0ba",
+    "url": "/SetgeliinMod/static/media/home.f0d5a2c5.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/SetgeliinMod/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/SetgeliinMod/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/SetgeliinMod/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/SetgeliinMod/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/SetgeliinMod/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "71b32866e83f08ea20c216cb4e0ccde1",
+    "url": "/SetgeliinMod/index.html"
+  }
+];
