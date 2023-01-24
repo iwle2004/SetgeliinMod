10bb4dbc62fe07c12739ac79c8e567b440efa614
Boldoo Zolbayar <bolderdenez40@gmail.com>
2023-01-24 14:10:40 +0800

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ function Home () {
     return (
 
     <section className="home">
-        <div className="container">
+        <div id="cont" className="container">
             <div className="home-cont">
                 <div className="home-1">
                     <h1 id="home-title">
