d5259d384d55c426b8f085aeacba6b03dbd7617f
iwle2004 <bilguunkhureltulga0@gmail.com>
2023-01-26 00:49:35 +0800

    :flame:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./App.css";
+import "./components/Nav.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from "./components/Nav";
+import Logo from "./images/logo.png"
+import {Nav, Navbar, Container} from 'react-bootstrap';
 import Home from "./components/Home";
 import About from "./components/About";
 import About2 from "./components/About2";
@@ -10,17 +12,50 @@ import Footer from "./components/Footer";
 
 
 
-
 function App() {
+
+  const home = useRef(null);
+  const about = useRef(null);
+  const contact = useRef(null);
+  
+  const scrollToSection = (elementRef) => {
+    console.log(elementRef);
+
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
+    <Navbar sticky='top' bg="white" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+            <img id='img-brand' src={Logo}></img>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="container-fluid">
+          <Navbar.Collapse className="justify-content-end">
+                <Nav.Link onClick={() => scrollToSection(home)}>Нүүр</Nav.Link>
+                <Nav.Link onClick={() => scrollToSection(about)}>Бидний тухай</Nav.Link>
+                <Nav.Link onClick={() => scrollToSection(contact)}>Холбогдох</Nav.Link>
+                <Nav.Link>Элсэх</Nav.Link>
+                <Nav.Link>
+                    <button type="button" class="btn btn-outline ">Donation</button>
+                </Nav.Link>
+            </Navbar.Collapse>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     <React.Fragment>
-      <Nav />
-      <Home />
-      <About />
+      <Home ref={home}/>
+      <About ref={about}/>
       <About2 />
       <About3 />
-      <Footer />
+      <Footer ref={contact}/>
     </React.Fragment>
   
     </>
