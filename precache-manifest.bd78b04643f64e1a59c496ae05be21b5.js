b27a29ef4d7a3fe77ac6a65297c0279c6a58e024
Boldoo Zolbayar <bolderdenez40@gmail.com>
2023-03-11 04:11:57 +0800

    Updates

---
+++ precache-manifest.bd78b04643f64e1a59c496ae05be21b5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "31ee401e6fddce8f616e",
+    "url": "/SetgeliinMod/static/css/main.7e06ea33.chunk.css"
+  },
+  {
+    "revision": "31ee401e6fddce8f616e",
+    "url": "/SetgeliinMod/static/js/main.31ee401e.chunk.js"
+  },
+  {
+    "revision": "dbc81e38bbb5f6f418b7",
+    "url": "/SetgeliinMod/static/css/1.a055daf4.chunk.css"
+  },
+  {
+    "revision": "dbc81e38bbb5f6f418b7",
+    "url": "/SetgeliinMod/static/js/1.dbc81e38.chunk.js"
+  },
+  {
+    "revision": "0cd3043ac695b22d4278",
+    "url": "/SetgeliinMod/static/js/runtime~main.0cd3043a.js"
+  },
+  {
+    "revision": "c4cb883b08e588650f8e4cee91e58a7f",
+    "url": "/SetgeliinMod/static/media/image 3.c4cb883b.jpeg"
+  },
+  {
+    "revision": "f0d5a2c553787cb7ba79c52db4b6f0ba",
+    "url": "/SetgeliinMod/static/media/home.f0d5a2c5.svg"
+  },
+  {
+    "revision": "1b66e27a6c95581049b0be9f700a1ed5",
+    "url": "/SetgeliinMod/static/media/IMG_0005.1b66e27a.JPG"
+  },
+  {
+    "revision": "2a109e00288e430c8d6bdfa987e1a62f",
+    "url": "/SetgeliinMod/static/media/IMG_1024.2a109e00.jpg"
+  },
+  {
+    "revision": "1ddf52d5fc079772343056f9766afa86",
+    "url": "/SetgeliinMod/static/media/IMG_6637.1ddf52d5.jpg"
+  },
+  {
+    "revision": "5057e138a2d42aedef2d80dfab102ad3",
+    "url": "/SetgeliinMod/static/media/IMG_6791.5057e138.jpg"
+  },
+  {
+    "revision": "e98153d518f3d809ccbbe2a3794c490c",
+    "url": "/SetgeliinMod/static/media/IMG_6482.e98153d5.jpg"
+  },
+  {
+    "revision": "a4999a13004b6bec2fa78e784aba201a",
+    "url": "/SetgeliinMod/static/media/IMG_0004.a4999a13.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/SetgeliinMod/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/SetgeliinMod/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/SetgeliinMod/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/SetgeliinMod/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/SetgeliinMod/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b590b466ec80cf96f443fbdd2d449b40",
+    "url": "/SetgeliinMod/index.html"
+  }
+];
